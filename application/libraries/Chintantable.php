9515d33e3f74d83e58496726d3319a1c3479cb0b
Tushar <tushar@wohlig.com>
2015-04-21 17:21:21 +0530

    Chintan God

--- application/libraries/Chintantable.php
+++ application/libraries/Chintantable.php
@@ -95,28 +95,29 @@ class Chintantable {
         echo '<nav class="chintantablepagination"><ul class="pagination"></ul></nav>';
     }
     public function createsearch($title = "", $description = "") {
-        echo '<div class="row">
-                <div class="col-md-9">
-                    <h4>' . $title . '</h4>
-                    <h6 >' . $description . '</h6>
-                </div>
-                <div class="col-md-3">
-                <select class="maxrow form-control" style="
-    float: left;  width: 76px;
-"><option value="10">10</option><option value="20">20</option><option value="50">50</option><option value="100">100</option></select>
-                    <div class="input-group">
-                        <input type="text" class="form-control chintantablesearch" placeholder="Search">
-                        <span class="input-group-btn">
-        <button class="btn btn-default chintantablesearchgo" type="button">Go!</button>
-      </span>
-                    </div>
-                </div>
+        echo '<div class="loader">
+        <div class="dots">Loading...</div>
             </div>
-            <div class="loader" style="
-    width: 100%;
-    position: fixed;  background: rgba(255, 31, 31, 0.431373);
-    z-index: 1;  height: 100%;  top: 0px;  left: 0px;  padding: 10%;  font-size: 51px;  color: white;  text-align: center;
-"> Loading </div>';
+        <div class="panel-heading">
+    <h3 class="panel-title">' . $title . '</h3>
+</div>
+<div class="panel-body">
+    <div class="bootstrap-table">
+        <div class="fixed-table-toolbar">
+            <div class="columns columns-right btn-group pull-right">            
+            <button class="btn btn-default chintantablesearchgo" type="button">Go!</button>
+                <select class="maxrow form-control" style="float: left;  width: 76px;">
+                    <option value="10">10</option>
+                    <option value="20">20</option>
+                    <option value="50">50</option>
+                    <option value="100">100</option>
+                </select>
+            </div>
+        <div class="pull-right search">
+            <input class="form-control chintantablesearch" type="text" placeholder="Search">
+        </div>
+        </div>
+    </div>';
     }
     public function gethighstockjson($element1, $element2, $from, $where = "", $group = "", $having = "", $order = "", $limit = "", $otherselect = "") {
         if ($where == "") {
